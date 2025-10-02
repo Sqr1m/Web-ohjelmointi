2fca9663f1398cdbd0abf6e55376d9c007a6154c
Sqr1m <105579161+Sqr1m@users.noreply.github.com>
2025-10-02 09:18:16 +0300

    4th 5th

---
+++ Backend/php/hintalaskuri.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+<meta charset="UTF-8">
+<title>Hintalaskuri</title>
+</head>
+<body>
+<h1>Tuotteen hintatiedot</h1>
+<?php
+$tuotteen_name = "Sähköpotkulauta";
+$hinta_pcs = 349.90;
+$kappalemaara = 2;
+$alennusprosentti = 15;
+
+$valisumma = $hinta_pcs * $kappalemaara;
+$alennus_eur = $valisumma * ($alennusprosentti / 100);
+$loppusumma = $valisumma - $alennus_eur;
+
+echo "Tuote: " . $tuotteen_name . "<br>";
+echo "Kappalehinta: " . number_format($hinta_pcs, 2, ',', '') . " €<br>";
+echo "Määrä: " . $kappalemaara . " kpl<br>";
+echo "------------------<br>";
+echo "Välisumma: " . number_format($valisumma, 2, ',', '') . " €<br>";
+echo "Alennus (" . $alennusprosentti . " %): " . number_format($alennus_eur, 2, ',', '') . " €<br>";
+echo "-------------------<br>";
+echo "<b>Lopullinen hinta: " . number_format($loppusumma, 2, ',', '') . " €</b><br>";
+?>
+</body>
+</html>
